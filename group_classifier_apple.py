291048dddf5748820033d10b548461c38113b8a8
ainemt06 <ainemt06@gmail.com>
2026-03-28 16:41:58 -0400

    logistic regression tuning

--- group_classifier_apple.py
+++ group_classifier_apple.py
@@ -4,6 +4,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler, GridSearchCV, train_test_split
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+kfold = KFold(n_splits=5, shuffle=True, random_state=1)
 
 def preprocess_data(filepath='.\\Data\\train.csv'):
 
@@ -27,7 +30,7 @@ def svm_tuning(X, y):
     'kernel': ['rbf', 'poly', 'sigmoid']
     }
 
-    kfold = KFold(n_splits=5, shuffle=True, random_state=1)
+
 
     grid_search = GridSearchCV(SVC(), param_grid, cv=kfold,scoring='accuracy')
 
@@ -37,12 +40,24 @@ def svm_tuning(X, y):
     print("Best cross-val accuracy:", grid_search.best_score_)
 
 
+def logregression_tuning(X, y):
+    param_grid = {
+        'C': [0.1,1,10,100],
+        'l1_ratio': np.linspace(0,1, num=5, endpoint=True),
+        'solver': ['lbfgs', 'liblinear', 'newton-cg', 'newton-cholesky', 'sag', 'saga']
+    }
+
+    grid_search = GridSearchCV(LogisticRegression(), param_grid, cv=kfold, scoring='accuracy')
+
+    grid_search.fit(X, y)
+
+    print("Best parameters:", grid_search.best_params_)
+    print("Best cross-val accuracy:", grid_search.best_score_)
 
 
 
 
 def kfold(model, X, y):
-    kfold = KFold(n_splits=5, shuffle=True, random_state=1)
     scores = cross_val_score(model, X, y, cv=kfold, scoring='accuracy')
     return scores
 
